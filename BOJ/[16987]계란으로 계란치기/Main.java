2c31e2829d00d7b12c52c100bf101005967b30de
김혜진 <201802084@o.cnu.ac.kr>
2023-09-13 14:47:29 +0900

    [BOJ 16987] 계란으로 계란치기

---
+++ BOJ/[16987]계란으로 계란치기/Main.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	static int n, cnt, ans = 0;
+	static int [] s, w;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		n = Integer.parseInt(br.readLine());
+		s = new int[n];
+		w = new int[n];
+		for(int i=0; i<n; i++) {
+			String [] split = br.readLine().split(" ");
+			s[i] = Integer.parseInt(split[0]); // 내구도
+			w[i] = Integer.parseInt(split[1]); // 무게
+		}
+		
+		dfs(0, 0);
+		System.out.println(ans);
+	}
+	
+	private static void dfs(int idx, int cnt) {
+		if(idx == n) {
+			ans = Math.max(ans, cnt);
+			 return;
+		}
+		
+		if(s[idx] <= 0 ) {
+			dfs(idx+1, cnt);
+			return;
+		}
+		
+		boolean flag = true;
+		for(int i = 0 ; i<n; i++) {
+			if(i == idx) continue; // 들고있는 계란이면 넘어가기
+			if(s[i]<=0) continue; // 이미 깨진 계란
+			
+			flag = false;
+			s[i] -= w[idx];
+			s[idx] -= w[i];
+
+			// 둘 다 깨짐
+			if(s[idx] <= 0 && s[i] <= 0) dfs(idx+1, cnt+2);
+			//하나만 깨짐
+			else if(s[idx] <=0 || s[i] <= 0)  dfs(idx+1, cnt+1);
+			//둘 다 안깨짐
+			else dfs(idx+1, cnt);
+			
+			s[i] += w[idx];
+			s[idx] += w[i];
+		}
+		if(flag) dfs(n, cnt);
+	}
+	
+}
