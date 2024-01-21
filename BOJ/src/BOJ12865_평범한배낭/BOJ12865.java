2faa087eba1ac3f07a617a97d084677e219c60d6
김혜진 <hyejin9203@naver.com>
2024-01-21 23:38:51 +0900

    init:폴더 변경

--- BOJ/src/BOJ12865_평범한배낭/BOJ12865.java
+++ BOJ/src/BOJ12865_평범한배낭/BOJ12865.java
@@ -1,4 +1,30 @@
 package BOJ12865_평범한배낭;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class BOJ12865 {
+
+    static int N, K;
+
+    public static void main(String [] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String [] split = br.readLine().split("");
+
+        N = Integer.parseInt(split[0]);
+        K = Integer.parseInt(split[1]);
+
+        int [] W = new int[N];
+        int [] V = new int[N];
+
+        for(int i=0; i<N; i++){
+            split = br.readLine().split("");
+            W[i] = Integer.parseInt(split[0]);
+            V[i] = Integer.parseInt(split[1]);
+        }
+
+        
+    }
 }
